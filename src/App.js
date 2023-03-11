22aff60028bc216ec9fd2378d485604c632512bd
Suriyaprakash-Github <yoursgump@gmail.com>
2023-03-11 15:56:34 +0530

    login added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
 import "./App.css";
 import SignUp from "./components/SignUp/SignUp";
+import Welcome from "./pages/Welcome";
 
 function App() {
   return (
-    <div className="app">
-      <h1>Expense Tracker</h1>
-      <SignUp />
-    </div>
+    <>
+      <div className="app">
+        <Routes>
+          <Route>
+            <Route path="/welcome" exact element={<Welcome />}></Route>
+          </Route>
+
+          <Route>
+            <Route path="/signup" exact element={<SignUp />}></Route>
+          </Route>
+        </Routes>
+      </div>
+    </>
   );
 }
 
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,28 +1,70 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import classes from "./signUp.module.css";
+import { useNavigate } from "react-router-dom";
+import Welcome from "../../pages/Welcome";
 
 const SignUp = () => {
-  const email = useRef();
-  const password = useRef();
-  const confirmPassword = useRef();
+  const [isLogin, setIsLogin] = useState(false);
+  const redirect = useNavigate();
+
+  const emailEntered = useRef();
+  const passwordEntered = useRef();
+  const confirmPasswordEntered = useRef();
+
+  const loginSwitch = () => {
+    setIsLogin((prev) => !prev);
+  };
 
   const submitHandler = (e) => {
     e.preventDefault();
 
-    if (password.current.value === confirmPassword.current.value) {
-      const signupDetails = {
-        email: email.current.value,
-        password: password.current.value,
-      };
-      console.log(signupDetails);
+    if (!isLogin) {
+      if (
+        passwordEntered.current.value === confirmPasswordEntered.current.value
+      ) {
+        fetch(
+          "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC415gt6s-Bwh87A8Renvlz03AmmWUJqrw",
+          {
+            method: "POST",
+            body: JSON.stringify({
+              email: emailEntered.current.value,
+              password: passwordEntered.current.value,
+              returnSecureToken: true,
+            }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        )
+          .then((res) => {
+            if (res.ok) {
+              alert("Signup Successful");
+              return res.json();
+            } else {
+              return res.json().then((data) => {
+                let errorMessage = "Authentication failed!";
 
+                throw new Error(errorMessage);
+              });
+            }
+          })
+          .then((data) => {
+            console.log(data);
+          });
+
+        //reset input
+        emailEntered.current.value = "";
+        passwordEntered.current.value = "";
+        confirmPasswordEntered.current.value = "";
+      } else return alert("Enter Password Correctly");
+    } else {
       fetch(
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC415gt6s-Bwh87A8Renvlz03AmmWUJqrw",
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyC415gt6s-Bwh87A8Renvlz03AmmWUJqrw",
         {
           method: "POST",
           body: JSON.stringify({
-            email: email.current.value,
-            password: password.current.value,
+            email: emailEntered.current.value,
+            password: passwordEntered.current.value,
             returnSecureToken: true,
           }),
           headers: {
@@ -32,7 +74,9 @@ const SignUp = () => {
       )
         .then((res) => {
           if (res.ok) {
-            alert("Signup Successful");
+            // alert("Login Successful");
+            redirect("/welcome");
+
             return res.json();
           } else {
             return res.json().then((data) => {
@@ -47,63 +91,81 @@ const SignUp = () => {
         });
 
       //reset input
-      email.current.value = "";
-      password.current.value = "";
-      confirmPassword.current.value = "";
-    } else return alert("Enter Password Correctly");
+      emailEntered.current.value = "";
+      passwordEntered.current.value = "";
+    }
   };
 
   return (
     <>
       <div className={classes.signup_main_div}>
+        {!isLogin ? <h2>Sign Up</h2> : <h2>Login</h2>}
         <div>
           <form onSubmit={submitHandler} className={classes.signup_form_div}>
-            <h2>Sign Up</h2>
-
             <div className={classes.signup_form_elements}>
               <label className={classes.signup_form_label} htmlFor="email">
-                Email
+                Email:
               </label>
               <input
                 type="email"
                 id="email"
                 placeholder="Enter your Email"
-                ref={email}
+                ref={emailEntered}
+                required
               />
             </div>
 
             <div className={classes.signup_form_elements}>
               <label htmlFor="password" className={classes.signup_form_label}>
-                Password
+                Password:
               </label>
               <input
                 type="password"
                 id="password"
                 placeholder="Enter your Password"
-                ref={password}
+                ref={passwordEntered}
                 minLength="7"
+                required
               />
             </div>
-
-            <div className={classes.signup_form_elements}>
-              <label
-                htmlFor="confirm_password"
-                className={classes.signup_form_label}
-              >
-                Confirm Password
-              </label>
-              <input
-                type="password"
-                id="confirm_password"
-                placeholder="Re-Enter your Password"
-                ref={confirmPassword}
-              />
-            </div>
-            <button type="submit" className={classes.signup_form_button}>
-              Sign Up
-            </button>
+            {!isLogin && (
+              <div className={classes.signup_form_elements}>
+                <label
+                  htmlFor="confirm_password"
+                  className={classes.signup_form_label}
+                >
+                  Confirm Password:
+                </label>
+                <input
+                  type="password"
+                  id="confirm_password"
+                  placeholder="Re-Enter your Password"
+                  ref={confirmPasswordEntered}
+                  required
+                />
+              </div>
+            )}
+            {!isLogin ? (
+              <button type="submit" className={classes.signup_form_button}>
+                Sign Up
+              </button>
+            ) : (
+              <button type="submit" className={classes.signup_form_button}>
+                Login
+              </button>
+            )}
           </form>
         </div>
+        {!isLogin && (
+          <button onClick={loginSwitch} className={classes.signup_form_button}>
+            Already Have an Account?
+          </button>
+        )}
+        {isLogin && (
+          <button onClick={loginSwitch} className={classes.signup_form_button}>
+            First time Here?
+          </button>
+        )}
       </div>
     </>
   );
